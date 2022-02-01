b78f8e712f5c55dfe698590ee0a4831c01515f0b
keyszl <keyszl@hendrix.edu>
2022-01-31 23:38:50 -0600

    basic eval

---
+++ src/checkers/evaluators/basic.java
@@ -0,0 +1,12 @@
+package checkers.evaluators;
+
+import checkers.core.Checkerboard;
+
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+
+public class basic implements ToIntFunction<Checkerboard> {
+    public int applyAsInt(Checkerboard c) {
+        return c.numPiecesOf(c.getCurrentPlayer()) - c.numPiecesOf(c.getCurrentPlayer().opponent());
+    }
+}
